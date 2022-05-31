01f4a9c434a95b26fd30aac02463c29293a508f6
PabloSadura <pablosadura@gmail.com>
2022-05-31 09:38:54 -0300

    Subiendo clase 2

---
+++ Clase2.js
@@ -0,0 +1,12 @@
+const datosPersonales = {
+  nombre: "Pablo",
+  edad: 36,
+  desarrollador: true,
+  fecha_de_nacimiento: new Date(1986, 2, 11),
+  libro: {
+    autor: "J. K. Rowling",
+    titulo: "Fantastic Beast",
+    fecha: new Date(2001, 01, 23),
+    ulr: "https://books.google.com.ar/books/about/La_magia_desde_dentro_as%C3%AD_se_hizo_anima.html?id=2Oo0DQAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false",
+  },
+};
