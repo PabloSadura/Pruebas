b5ece59a1af9863ae9480b10508b624135c0af92
PabloSadura <pablosadura@gmail.com>
2022-06-13 08:45:25 -0300

    Clase 4

---
+++ Clase4/strings.js
@@ -0,0 +1,8 @@
+let nombre = "Pablo";
+let apellido = "Sadura";
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMin = estudiante.toLowerCase();
+let cantLetras = estudiante.length;
+let primerLetra = nombre.charAt(0);
+console.log(primerLetra);
