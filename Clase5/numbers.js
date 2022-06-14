11e3207fec2394892c301a0c62965c17b32fb526
PabloSadura <pablosadura@gmail.com>
2022-06-14 07:06:14 -0300

    entrega Numbers

---
+++ Clase5/numbers.js
@@ -0,0 +1,6 @@
+let altura_cm = 183;
+let altura_m = 1.83;
+let peso_kg = 93.5;
+let altura_redondeada = altura_m.toFixed(0);
+console.log(altura_redondeada);
+let peso_redondeado = Math.floor(peso_kg);
